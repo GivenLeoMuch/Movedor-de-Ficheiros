f48830be2036c96b6740ba092aaa71e89d3f97a3
Given LeoMuch <givenleomuch@gmail.com>
2025-08-25 17:34:07 +0200

    Adicionei linhas de comentarios para algumas sessoes do codigo para poder saber onde esta cada funcao

--- Movedor de Ficheiros/Program.cs
+++ Movedor de Ficheiros/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace NomeQualquer
+{
+    namespace NomeQualquer
+    {
+        class Programa
+        {
+            static void Main(string[] args)
+            {
+                //Definição dos diretorios de origem e destino.
+                string origem = @"D:\Origem";
+                string destino = @"D:\Destino";
+
+                //Parte responsável por mover os ficheiros.
+                foreach (var dir in Directory.GetDirectories(origem))
+                {
+                    string nome = Path.GetFileName(dir);
+                    string destinoFinal = Path.Combine(destino, nome);
+
+                    Directory.Move(dir, destinoFinal);
+                    Console.WriteLine("Ficheiros movidos com sucesso!");
+                }
+            }
+        }
+    }
+}
