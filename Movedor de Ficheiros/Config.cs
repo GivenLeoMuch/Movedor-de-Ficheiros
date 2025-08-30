7200fe52a83abcf77eb4cb4a9f9bd677b231c2de
Given LeoMuch <givenleomuch@gmail.com>
2025-08-30 19:19:34 +0200

    Definicao e leitura do ficheiro Json no programa principal

---
+++ Movedor de Ficheiros/Config.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Movedor_de_Ficheiros
+{
+     class Config
+    {
+        public string Origem { get; set; }
+        public string Destino { get; set; }
+    }
+}
--- Movedor de Ficheiros/Program.cs
+++ Movedor de Ficheiros/Program.cs
@@ -1,22 +1,35 @@
 ﻿using System;
 using System.IO;
+using Movedor_de_Ficheiros;
 using Newtonsoft.Json;
 
     namespace FileMover
     {
         class Programa
         {
+            
+         
             static void Main(string[] args)
             {
-                //Definição dos diretorios de origem e destino.
-                string origem = @"D:\Origem";
-                string destino = @"D:\Destino";
+
+            string caminhoConfig = "C:\\Mr Robot\\Programming\\Movedor de Ficheiros\\Movedor de Ficheiros\\Movedor.json";
+
+            // Lê o ficheiro
+            string json = File.ReadAllText(caminhoConfig);
+
+            // Converte em objeto
+            Config config = JsonConvert.DeserializeObject<Config>(json);
+
+            // Usa os valores do JSON
+            Console.WriteLine("Origem: " + config.Origem);
+            Console.WriteLine("Destino: " + config.Destino);
+
 
                 //Parte responsável por mover os ficheiros.
-                foreach (var dir in Directory.GetDirectories(origem))
+                foreach (var dir in Directory.GetDirectories(config.Origem))
                 {
                     string nome = Path.GetFileName(dir);
-                    string destinoFinal = Path.Combine(destino, nome);
+                    string destinoFinal = Path.Combine(config.Destino, nome);
 
                     Directory.Move(dir, destinoFinal);
                     Console.WriteLine("Ficheiros movidos com sucesso!");
